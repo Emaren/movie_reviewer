44e49aa4f2abc9186e1f5144a4930ff1f678f832
Tony Blum <tonyblum@me.com>
2015-11-15 11:15:49 -0800

    image integration

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,68 @@
+class MoviesController < ApplicationController
+
+  before_action :find_movie, only: [:show, :edit, :update, :destroy]
+
+      def index
+        if params[:category].blank?
+        @movies = Movie.all.order("created_at DESC")
+      else
+        @category_id = Category.find_by(name: params[:category]).id
+        @movies = Movie.where(:category_id => @category_id).order("created_at DESC")
+      end
+  end
+
+      def show
+
+      end
+
+      def new
+        @movie = current_user.movies.build
+        @categories = Category.all.map { |c| [c.name, c.id] }
+      end
+
+      def create
+        @movie = current_user.movies.build movie_params
+        @movie.category_id = params[:category_id]
+        if @movie.save
+          redirect_to root_path
+        else
+          render 'new'
+        end
+      end
+
+      def edit
+        @categories = Category.all.map { |c| [c.name, c.id] }
+
+      end
+
+      def update
+        @movie.category_id = params[:category_id]
+
+        if @movie.update movie_params
+          redirect_to movie_path(@movie)
+        else
+          render 'edit'
+        end
+
+      end
+
+      def destroy
+        if @movie.destroy
+          redirect_to root_path
+        else
+          render
+        end
+      end
+
+
+      private
+
+      def movie_params
+        params.require(:movie).permit(:title, :description, :director, :category_id, :movie_img)
+      end
+
+      def find_movie
+        @movie = Movie.find params[:id]
+      end
+
+    end
---
+++ app/models/movie.rb
@@ -0,0 +1,12 @@
+class Movie < ActiveRecord::Base
+
+  belongs_to :user
+
+  belongs_to :category
+
+
+  has_attached_file :movie_img, :styles => { :movie_index => "250x350>", :movie_show => "325x475>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :movie_img, :content_type => /\Aimage\/.*\Z/
+
+
+end
